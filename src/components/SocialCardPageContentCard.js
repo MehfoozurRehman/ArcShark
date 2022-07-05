6dcaaecdb09a2835dcee96a9379f2dcaa890d717
Bilal Shabbir <102783392+IBilalShabbir@users.noreply.github.com>
2022-07-05 18:37:35 +0500

    socialcards fixes

--- src/components/SocialCardPageContentCard.js
+++ src/components/SocialCardPageContentCard.js
@@ -12,7 +12,7 @@ export function SocialCardPageContentCard({}) {
   return (
     <a href="#" className="home__content__card__entry">
       <div className="home__content__entry__content">
-        <img src={user} alt="user" />
+        <img src={user} width={60} height={60} alt="user" />
       </div>
       <div className="home__content__card__entry__user__detail">
         <div className="home__content__card__entry__name__heading">
@@ -34,7 +34,7 @@ export function SocialCardPageContentCard({}) {
               data-name="Icon material-location-on"
               d="M14.121,3C10.46,3,7.5,5.6,7.5,8.823c0,4.367,6.621,10.813,6.621,10.813s6.621-6.446,6.621-10.813C20.741,5.6,17.781,3,14.121,3Zm0,7.9a2.237,2.237,0,0,1-2.364-2.08,2.237,2.237,0,0,1,2.364-2.08,2.237,2.237,0,0,1,2.364,2.08A2.237,2.237,0,0,1,14.121,10.9Z"
               transform="translate(-7.5 -3)"
-              fill="#5b58a5"
+              fill="currentColor"
             />
           </svg>
           <div className="home__content__card__entry__location__country__name">
