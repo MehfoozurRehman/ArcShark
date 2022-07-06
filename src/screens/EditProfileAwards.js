5b603cd6d9da93263fdfc422f6c2ca39fbcb1c9f
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2022-07-06 16:49:29 +0500

    EditProfileAwards done

--- src/screens/EditProfileAwards.js
+++ src/screens/EditProfileAwards.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Plus } from "react-feather";
 import SelectInput from "../components/SelectInput";
 import TextareaInput from "../components/TextareaInput";
 import TextInput from "../components/TextInput";
@@ -14,84 +15,30 @@ export default function EditProfileAwards() {
       </div>
       <div className="dashbaord__content__main__content">
         <div className="dashbaord__content__main__content__row">
-          <TextInput placeholder="First Name" />
+          <TextInput placeholder="Organization Name" />
         </div>
         <div className="dashbaord__content__main__content__row">
           <div className="dashbaord__content__main__content__col">
-            <TextInput placeholder="First Name" />
+            <TextInput placeholder="Award" />
           </div>
-          <div className="dashbaord__content__main__content__col">
-            <TextInput placeholder="Last Name" />
-          </div>
-        </div>
-        <div className="dashbaord__content__main__content__row">
-          <div className="dashbaord__content__main__content__col">
-            <SelectInput
-              placeholder="City"
-              options={[
-                { value: "chocolate", label: "Chocolate" },
-                { value: "strawberry", label: "Strawberry" },
-                { value: "vanilla", label: "Vanilla" },
-              ]}
-            />
-          </div>
-          <div className="dashbaord__content__main__content__col">
-            <SelectInput
-              placeholder="Country"
-              options={[
-                { value: "chocolate", label: "Chocolate" },
-                { value: "strawberry", label: "Strawberry" },
-                { value: "vanilla", label: "Vanilla" },
-              ]}
-            />
-          </div>
-        </div>
-        <div className="dashbaord__content__main__content__row">
           <div className="dashbaord__content__main__content__col">
             <TextInput placeholder="Website" />
           </div>
-          <div className="dashbaord__content__main__content__col">
-            <TextInput placeholder="Occupation" />
-          </div>
-        </div>
-        <div className="dashbaord__content__main__content__row">
-          <TextInput placeholder="About" />
-        </div>
-        <div className="dashbaord__content__main__content__row">
-          <TextareaInput placeholder="Description" />
-        </div>
-        <div className="dashbaord__content__main__content__row">Socials</div>
-        <div className="dashbaord__content__main__content__row">
-          <div className="dashbaord__content__main__content__col">
-            <TextInput placeholder="Facebook" />
-          </div>
-          <div className="dashbaord__content__main__content__col">
-            <TextInput placeholder="Instagram" />
-          </div>
         </div>
         <div className="dashbaord__content__main__content__row">
           <div className="dashbaord__content__main__content__col">
-            <TextInput placeholder="Twitter" />
+            <TextInput placeholder="Date Recieved" />
           </div>
-          <div className="dashbaord__content__main__content__col">
-            <TextInput placeholder="Youtube" />
-          </div>
+          <div
+            className="dashbaord__content__main__content__col"
+            style={{ marginBottom: 0 }}
+          />
         </div>
         <div className="dashbaord__content__main__content__row">
-          <div className="dashbaord__content__main__content__col">
-            <TextInput placeholder="Dribble" />
-          </div>
-          <div className="dashbaord__content__main__content__col">
-            <TextInput placeholder="Behance" />
-          </div>
-        </div>
-        <div className="dashbaord__content__main__content__row">
-          <div className="dashbaord__content__main__content__col">
-            <TextInput placeholder="Telegram" />
-          </div>
-          <div className="dashbaord__content__main__content__col">
-            <TextInput placeholder="Pintrest" />
-          </div>
+          <button className="dashbaord__content__main__content__row__button">
+            <Plus size={20} color="currentColor" />
+            Add Award
+          </button>
         </div>
       </div>
     </>
